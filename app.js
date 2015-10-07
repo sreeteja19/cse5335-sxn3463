4a8ec30f4ac6149b6d9ab3415c4411e21d9d72a7
sreeteja19 <nannapaneni.sreeteja@gmail.com>
2015-10-07 01:10:22 +0000

    new

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.locals.proj = require('./db.json');
+var proj = require('./db.json');
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
